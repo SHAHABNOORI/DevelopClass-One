12232332616a09be917e8ccf6286eeedd3c7eb47
shahabnoori <noorigoodarzi@gmail.com>
2020-05-04 19:10:16 +0430

    Commit hello world sample

--- ClassOne/DeveloperClass-1/Program.cs
+++ ClassOne/DeveloperClass-1/Program.cs
@@ -6,7 +6,12 @@ namespace DeveloperClass_1
     {
         static void Main(string[] args)
         {
+           /* int tike code yek matn 
+                ra roye safhe chap 
+                mikonad */
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
+
