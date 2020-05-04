f650cbf11935a14daf29501cf5e61ed3550bba8d
shahabnoori <noorigoodarzi@gmail.com>
2020-05-04 18:53:35 +0430

    Initial Project

---
+++ ClassOne/DeveloperClass-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveloperClass_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
