9773e46c7df16b7b1d5a9b049c9141fd64bad84c
shahabnoori <noorigoodarzi@gmail.com>
2020-05-04 19:37:32 +0430

    Commit Refrence type value type

--- ClassOne/DeveloperClass-1/Program.cs
+++ ClassOne/DeveloperClass-1/Program.cs
@@ -6,12 +6,35 @@ namespace DeveloperClass_1
     {
         static void Main(string[] args)
         {
-           /* int tike code yek matn 
-                ra roye safhe chap 
-                mikonad */
-            Console.WriteLine("Hello World!");
+            /* int tike code yek matn 
+                 ra roye safhe chap 
+                 mikonad */
+
+            //int x = 10;
+            //int y = x;
+            //y = y + 10;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+
+            Calculator cOne = new Calculator() { X = 10, Y = 20 };
+            Calculator cTwo = cOne;
+
+            cTwo.X = 100;
+            cTwo.Y = 250;
+
+            Console.WriteLine(cOne.X);
+            Console.WriteLine(cOne.Y);
+
             Console.ReadKey();
         }
+    }
+
+    class Calculator
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
     }
 }
 
