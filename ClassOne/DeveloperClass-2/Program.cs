8870e71b8d3d5af64c026f5f6cb4c9aa8219673a
shahabnoori <noorigoodarzi@gmail.com>
2020-05-09 19:04:42 +0430

    Initial Operators and operand

--- ClassOne/DeveloperClass-2/Program.cs
+++ ClassOne/DeveloperClass-2/Program.cs
@@ -6,7 +6,36 @@ namespace DeveloperClass_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int numOne = 20;
+
+            //double numTwo = 20.6565656;
+
+            //float numThree = 20.6f;
+
+            //bool shart = true;
+
+            //string fullName = "Shahab Noori Goodarzi";
+
+            //char ch = 'a';
+
+            //byte b = 255;
+
+            //var test = 20.85f;
+
+            //int a = 30;
+            //int b = 30;
+
+            //var result = (a <= b);
+
+            //var result = (-(1) + 6 / 2 * 7 * 3 - 3 + (2 * 6) * 3) == (2 + 60);
+
+            //var result = a++;
+            //var result = ++a;
+            //var result = a--;
+
+            //Console.WriteLine(result);
+            //Console.WriteLine(a);
         }
     }
 }
+
