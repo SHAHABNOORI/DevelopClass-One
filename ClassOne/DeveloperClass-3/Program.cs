2992e0c3512827f6dfb393f1be9b01c1286a9adf
shahabnoori <noorigoodarzi@gmail.com>
2020-05-14 20:00:09 +0430

    finish

--- ClassOne/DeveloperClass-3/Program.cs
+++ ClassOne/DeveloperClass-3/Program.cs
@@ -6,7 +6,75 @@ namespace DeveloperClass_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+            //for (; i < 9; i = i + 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("-------------------------");
+            //for (i=101; i < 1000; i = i + 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("-------------------------");
+            //for (i = 100; i < 991; i++)
+            //{
+            //    if (i % 2 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (i = 100; i < 991; i = i + 10)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int j = 8; j >= 0; j=j-2)
+            //{
+            //    Console.WriteLine(j);
+            //}
+            //char charavtereman = '*';
+            //for (int j = 0; j < 5; j++)
+            //{
+            //    for (int k = 0; k <= j; k++)
+            //    {
+            //        Console.Write(charavtereman);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //i = 0;
+            //while (i <= 8)
+            //{
+            //    Console.WriteLine(i);
+            //    i = i + 2;
+            //}
+
+            //i = 20;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i = i + 2;
+            //} while (i<=8);
+
+
+            int a = 100;
+            int b = 20;
+            var result = a < b ? "a" : "b";
+            Console.WriteLine(result);
+            //Console.WriteLine(a < b ? "----" : "+++++");
         }
     }
 }
+
+//*
+//**
+//***
+//****
+//*****
+
+
+//1 2 3 4 5 6 7 8 9 10
+//2 4 6 8 10 12 14 16 18 20
+//3 6 9 12 15 18
