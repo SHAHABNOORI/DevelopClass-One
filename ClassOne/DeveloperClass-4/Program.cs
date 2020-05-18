10ce172c39a7acf16b2a40a8d818dc7da63a85ac
shahabnoori <noorigoodarzi@gmail.com>
2020-05-18 22:33:07 +0430

    finish

--- ClassOne/DeveloperClass-4/Program.cs
+++ ClassOne/DeveloperClass-4/Program.cs
@@ -67,8 +67,102 @@ namespace DeveloperClass_4
 
             #endregion
 
+            //Console.WriteLine(CalculateTax(5000000));
 
+            #region Calculator
+
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(Add(x, y));
+
+            #endregion
+
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(CustomeCalculator(x, y,'+'));
+
+
+
+            //for (int i = 1; i < 1000; i++)
+            //{
+            //    if ((i % 7 == 0) && (i % 3 == 0) && (i % 9 == 0))
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (j == 5)
+                    {
+                        break;
+                    }
+                    Console.Write(i*j+"\t");
+                }
+                Console.WriteLine();
+            }
 
         }
+
+        static int CustomeCalculator(int x, int y, char operation)
+        {
+            int result = 0;
+            switch (operation)
+            {
+                case '*':
+                    result = x * y;
+                    break;
+                case '/':
+                    result = x / y;
+                    break;
+                case '+':
+                    result = x + y;
+                    break;
+                case '-':
+                    result = x - y;
+                    break;
+            }
+
+            return result;
+        }
+
+        #region Calculator Methods
+
+        static int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        static int Sub(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+
+        static int Mult(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+
+        static int Division(int numberOne, int numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+
+        static int CalculateTax(int salary)
+        {
+            int result = (salary / 100) * 9;
+            return result;
+        }
+
+
+        #endregion
     }
 }
+
+
+//method signature
