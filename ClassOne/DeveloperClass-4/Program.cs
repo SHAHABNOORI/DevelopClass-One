b52e929bce772409a1ff798d7a9231612936a38d
shahabnoori <noorigoodarzi@gmail.com>
2020-05-18 21:31:23 +0430

    Initial DeveloperClass-4 with samples

---
+++ ClassOne/DeveloperClass-4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace DeveloperClass_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Adad Aval
+
+            //int myNum = 0;
+            //myNum = Convert.ToInt32(Console.ReadLine());
+
+            //if (myNum == 1 || myNum == 2 || myNum == 3 || myNum == 5 || myNum == 7)
+            //{
+            //    Console.WriteLine("Adad Aval Ast");
+            //}
+            //else if ((myNum % 2 != 0) && (myNum % 3 != 0) && (myNum % 5 != 0) && (myNum % 7 != 0))
+            //{
+            //    Console.WriteLine("Adad Aval Ast");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Adad Aval Nist");
+            //}
+
+            #endregion
+
+            #region Jadval Zarb
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.Write(i*j +"\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region SwitchCase Sample
+
+            //int myNum = Convert.ToInt32(Console.ReadLine());
+
+            //switch (myNum)
+            //{
+            //    case 1:
+            //        Console.WriteLine("One");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Three");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Four");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Five");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is out of range");
+            //        break;
+            //}
+
+            #endregion
+
+
+
+        }
+    }
+}
