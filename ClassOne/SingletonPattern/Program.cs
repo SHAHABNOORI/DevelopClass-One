25a892f0ac6dbbfdc981b4fae19bfa449d71acfc
shahabnoori <noorigoodarzi@gmail.com>
2020-07-31 09:37:51 +0430

    Initial Singlton Pattern

---
+++ ClassOne/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SiteSetting setting = SiteSetting.GetInstance();
+
+            Console.WriteLine(setting.SiteName);
+        }
+    }
+}
---
+++ ClassOne/SingletonPattern/SiteSetting.cs
@@ -0,0 +1,19 @@
+﻿namespace SingletonPattern
+{
+    public class SiteSetting
+    {
+        private static SiteSetting _instance = new SiteSetting();
+       
+        public static SiteSetting GetInstance()
+        {
+            return _instance;
+        }
+
+        public string SiteName { get; set; } = "Hello World";
+
+        private SiteSetting()
+        {
+
+        }
+    }
+}
